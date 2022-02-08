ebfaaa854651d48f16076db404afb26b3f1116ae
megamagomed <megamagomed1@gmail.com>
2022-02-08 17:02:01 +0300

    magomed molodec. genialno reshil zadachu

--- task1.js
+++ task1.js
@@ -1,1 +1,5 @@
-console.log('!!!!!!!!!');
+function createPhoneNumber(numbers) {
+    return '(' + numbers[0] + numbers[1] + numbers[2] + ')'
+}
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890"
